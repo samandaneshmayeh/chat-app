5616e697ba3ade0e55b6f115187ea0463cea65d8
Saman Daneshmayeh <daneshmayeh.saman@gmail.com>
2023-04-16 21:15:36 +0430

    context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { Switch, Route } from "react-router-dom";
 
 // Components
 import Login from "./components/Login";
+import Chats from "./components/Chats"
+
+// Context
+import AuthContextProvider from "./context/AuthContextProvider";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/chats" component={Chats} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </AuthContextProvider>
     </div>
   );
 }
---
+++ src/context/AuthContextProvider.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
+
+const authContext = React.createContext();
+
+const AuthContextProvider = ({children}) => {
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(false);
+    const history = useHistory();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            setUser(user);
+            setLoading(false);
+            if(user) history.push("/chats");
+        })
+    }, [user, history])
+
+    return (
+        
+            <authContext.Provider value={user}>
+                {!loading && children}
+            </authContext.Provider>
+        
+    );
+};
+
+export default AuthContextProvider;
